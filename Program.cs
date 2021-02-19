f65101612d2969f6014a0c58c96edab69a7e8e67
lipun45dora <64270114+lipun45dora@users.noreply.github.com>
2021-02-19 17:30:14 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,50 @@
 ﻿using System;
 
-namespace star2
+namespace Array
 {
     class Program
     {
         static void Main(string[] args)
-        { int row=5;
-            for(int i=0;i<row;i++)
-            {
-                for(int j=2*(row-i); j>=0; j--)
-                {
-                    Console.Write(" ");
-                }
-                for(int k=0;k<=i;k++)
-                {
-                //Console.WriteLine();
-                Console.Write("* ");
-                }
-                Console.WriteLine();
-              
-              //Console.WriteLine();
-            }
+        {
+         //Array Intialization
+          string[] car={"Audi","Bmw","Toyota","Tata"};
+          int[] roll={010,0233,33,44,55,86};
+
+          // acessing array element
+          Console.WriteLine(car[2]);
+          Console.WriteLine(roll[4]);
+
+          //Looping an array
+          for(int i=0;i<car.Length;i++)
+          {
+            Console.WriteLine(car[i]);
+          }
+          for(int j=0;j<roll.Length;j++)
+          {
+            Console.WriteLine(roll[j]);
+          }
+
+          //looping using foreach
+          foreach(string a in car)
+           {
+            Console.WriteLine(a);
+          }
+          foreach(int b in roll)
+           {
+            Console.WriteLine(b);
+          }
+
+          //Array sorting
+        /*  Array.Sort(car);
+foreach (string c in car)
+{
+  Console.WriteLine(c);
+}
+Array.Sort(roll);
+foreach (int d in roll)
+{
+  Console.WriteLine(d);
+}*/
         }
     }
 }
